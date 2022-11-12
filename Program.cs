850bdcac4dd36318da683b134f10b52408d71dc5
a.artamonov <a.artamonov@gb.ru>
2022-11-12 14:27:31 +0300

    All is commented

--- Program.cs
+++ Program.cs
@@ -94,58 +94,58 @@
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-double [] CreateNewArray(int size)
-{
-    double [] array = new double[size];
-    for(int index = 0; index < size; index++)
-    {
-        array[index] = new Random().Next(-9, 10) + new Random().NextDouble();
-    }
-    return array;
-}
+// double [] CreateNewArray(int size)
+// {
+//     double [] array = new double[size];
+//     for(int index = 0; index < size; index++)
+//     {
+//         array[index] = new Random().Next(-9, 10) + new Random().NextDouble();
+//     }
+//     return array;
+// }
 
-void ShowArray(double [] array)
-{
-    Console.Write("[");
-    for (int index = 0; index < array.Length; index++)
-    {
-        Console.Write(array[index] + "; ");
-    }
-    Console.Write("\b\b]");
-    Console.WriteLine();
-}
+// void ShowArray(double [] array)
+// {
+//     Console.Write("[");
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         Console.Write(array[index] + "; ");
+//     }
+//     Console.Write("\b\b]");
+//     Console.WriteLine();
+// }
 
-double FindMaxElemArray (double [] array)
-{
-    double max = array[0];
-    for(int index = 1; index < array.Length; index++)
-    {
-        if(array[index] > max) max = array[index];
-    }
-    return max;
-}
+// double FindMaxElemArray (double [] array)
+// {
+//     double max = array[0];
+//     for(int index = 1; index < array.Length; index++)
+//     {
+//         if(array[index] > max) max = array[index];
+//     }
+//     return max;
+// }
 
-double FindMinElemArray (double [] array)
-{
-    double min = array[0];
-    for(int index = 1; index < array.Length; index++)
-    {
-        if(array[index] < min) min = array[index];
-    }
-    return min;
-}
+// double FindMinElemArray (double [] array)
+// {
+//     double min = array[0];
+//     for(int index = 1; index < array.Length; index++)
+//     {
+//         if(array[index] < min) min = array[index];
+//     }
+//     return min;
+// }
 
-Console.Write("Input a size of array: ");
-int size = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a size of array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-if (size > 0)
-{
-    double [] randArray = CreateNewArray(size);
-    ShowArray(randArray);
-    double maxElem = FindMaxElemArray(randArray);
-    double minElem = FindMinElemArray(randArray);
-    Console.WriteLine($"Max: {maxElem}");
-    Console.WriteLine($"Min: {minElem}");
-    Console.WriteLine($"Diff of the max and min elements of array is {maxElem - minElem}");
-}
-else Console.WriteLine("Size of array must be greater than 0. Try Again!");
+// if (size > 0)
+// {
+//     double [] randArray = CreateNewArray(size);
+//     ShowArray(randArray);
+//     double maxElem = FindMaxElemArray(randArray);
+//     double minElem = FindMinElemArray(randArray);
+//     Console.WriteLine($"Max: {maxElem}");
+//     Console.WriteLine($"Min: {minElem}");
+//     Console.WriteLine($"Diff of the max and min elements of array is {maxElem - minElem}");
+// }
+// else Console.WriteLine("Size of array must be greater than 0. Try Again!");
